a9a62bc8211ab0cb4b9c727b3fcbc5b9c5463b35
Fakehex <fullgcdhacked@outlook.fr>
2019-10-28 14:51:45 +0100

    debut de Nouvelle tier liste, creation du premier fragment

---
+++ app/src/main/java/com/example/tierliste3/Controllers/Activities/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tierliste3.Controllers.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.tierliste3.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //lance une nouvelle activity pour crée une liste
+    public void nouvelleTierListe(View view){
+        Intent intent = new Intent(this, NouvelleTierListeActivity.class);
+        startActivity(intent);
+    }
+}
