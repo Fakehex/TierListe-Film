c19402beb5dc6e9012085ca9e4ed0cb31e65549b
Quentin <quentin.legret@etu.univ-orleans.fr>
2019-10-28 18:01:50 +0100

    Version 2

--- app/src/main/java/com/example/tierliste3/Controllers/Activities/ListeTiersActivity.java
+++ app/src/main/java/com/example/tierliste3/Controllers/Activities/ListeTiersActivity.java
@@ -10,7 +10,9 @@ public class ListeTiersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_tiers);
+
     }
 }
